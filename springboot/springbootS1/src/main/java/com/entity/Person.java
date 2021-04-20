a6a9ef23b3ec12b168bbe29903f57273a5553893
ljx <1908360195@qq.com>
2021-04-20 20:21:52 +0800

    springboot学习 主要是属性文件

---
+++ springboot/springbootS1/src/main/java/com/entity/Person.java
@@ -0,0 +1,50 @@
+package com.entity;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+/**
+ * @ClassName : Person
+ * @Author : ljx
+ * @Date: 2021/4/20 18:57
+ * @Description :
+ */
+@Component
+public class Person {
+    @Value("${name}")
+    private String name;
+    @Value("${age}")
+    private Integer age;
+
+    public Person(String name, Integer age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public Person() {
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+    @Override
+    public String toString() {
+        return "Person{" +
+                "name='" + name + '\'' +
+                ", age=" + age +
+                '}';
+    }
+}
