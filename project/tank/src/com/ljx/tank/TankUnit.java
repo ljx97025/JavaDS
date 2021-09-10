a2ec725ceafa0b27ae2cdad34388852082e0db44
ljx <1908360195@qq.com>
2021-09-11 00:14:13 +0800

    坦克大战--坦克群组创建

---
+++ project/tank/src/com/ljx/tank/TankUnit.java
@@ -0,0 +1,14 @@
+package com.ljx.tank;
+
+/**
+ * @ClassName : TankUnit
+ * @Author : ljx
+ * @Date: 2021/9/11 0:11
+ * @Description : 坦克的群组，用于管理坦克群组操作
+ */
+public class TankUnit {
+    private Tank[] tanks;
+
+
+
+}
