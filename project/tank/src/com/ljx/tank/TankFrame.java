ee37db0f865378df8e6ee2b2e6dcbf04959cacaf
ljx <1908360195@qq.com>
2021-11-07 13:45:09 +0800

    【任务】：gitee 测试使用
    【修改内容】：仅用于gitee使用测试

--- project/tank/src/com/ljx/tank/TankFrame.java
+++ project/tank/src/com/ljx/tank/TankFrame.java
@@ -83,7 +83,7 @@ public class TankFrame extends Frame {
                     break;
                 case KeyEvent.VK_CONTROL:
                     gm.getMainTank().fire();
-//                    gm.getMainTank().handleFireKey();
+//                    gm.getMainTank().handleFireKey();   // 观察者模式
                     break;
             }
 
